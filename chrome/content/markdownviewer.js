58f13480943c83127e9dfc489c8966dc5cc82aba
U-R03445\LWH <LWH@R03445.ruijie.com.cn>
2014-04-04 14:29:34 +0800

    version 2.0

--- chrome/content/markdownviewer.js
+++ chrome/content/markdownviewer.js
@@ -19,31 +19,30 @@ if (typeof markdownviewer === 'undefined') {
 
 			if (regexpMdFile.test(document.location)) {
 				marked.setOptions({
-					gfm: true,
-					pedantic: false,
-					sanitize: false,
-					langPrefix: 'language-'
+					langPrefix: 'prettyprint linenums lang-'
 				});
 
 				var content = document.firstChild;
 				content.innerHTML = '<!DOCTYPE html>' +
 				                    '<head>' +
+                                    '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" >'+
 				                    '    <title>Markdown Viewer</title>' +
-				                    '    <link rel="stylesheet" type="text/css" href="resource://mdskin/bootstrapLite.css">' +
-						    '    <link rel="stylesheet" type="text/css" href="resource://mdskin/default.min.css">' +
+				                    '    <link rel="stylesheet" type="text/css" href="resource://mdskin/bootstrap.css">' +
+						    '    <link rel="stylesheet" type="text/css" href="resource://mdskin/jquery.jqTOC.css">' +
+						    '    <link rel="stylesheet" type="text/css" href="resource://mdskin/prettify.css">' +
 				                    '</head>' +
 				                    '<body class="container">' +
 				                        marked(content.textContent) +
 				                    '</body>';
 				var loadjsfile=function(doc, jsfile){
+                    console.log('loadjsfile %o', jsfile);
 					var script = doc.createElement("script");
 					script.type = "text/javascript";
 					script.src = "resource://mdskin/"+jsfile;
 					var head = doc.getElementsByTagName("head")[0] || doc.documentElement;
 					head.insertBefore(script, head.firstChild);
 				};
-				loadjsfile(document, 'highlight.min.js');
-				loadjsfile(document, 'inithighlight.js'); 
+				loadjsfile(document, 'bootall.js');
 			}
 		}
 	};
